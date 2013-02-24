d8b045a93dd9aff8a1f02a242a49f89205c11085
Nick Stephens <nick.stephens93@gmail.com>
2013-02-24 00:02:49 -0800

    Updated the clc shellscript

--- Clite-student/clc.sh
+++ Clite-student/clc.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+wd=$(pwd)
+cwd=$(dirname "$0")
+
 java CodeGen $1 > /dev/null
 
 # Some bash string operations
@@ -7,5 +11,6 @@ EXTEN_LEN=${#EXTEN}
 
 # This operation cuts off the .cpp extension
 jasm=${1:0:$(expr ${#1} - $EXTEN_LEN)} 
+echo "$jasm"
 
 java -jar /Users/Nick/bin/jasmin.jar $jasm.j > /dev/null
