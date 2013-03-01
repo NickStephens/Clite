8e5dee05dc546ac9fdab4c944aba4256de6f3cba
Nick Stephens <nick.stephens93@gmail.com>
2013-02-28 22:04:37 -0800

    modified and cleaned up clc.sh

--- clc.sh
+++ clc.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 if [[ $1 == "-i" ]]
-   then java -jar bin/CliteInterpreter.jar $2
+   then java -jar ~/.clite/CliteInterpreter.jar $2
 else 
-if [[ $1 == "-c" ]] 
-   then
-   java -jar bin/CliteCompiler.jar $2 > /dev/null
+   if [[ $1 == "-c" || $1 == "-cj"]] 
+	then
+	java -jar ~/.clite/CliteCompiler.jar $2 > /dev/null
 
-   # Some bash string operations
-   EXTEN=".cpp"
-   EXTEN_LEN=${#EXTEN}
+	# Some bash string operations
+	EXTEN=".cpp"
+	EXTEN_LEN=${#EXTEN}
 
-   # This operation cuts off the .cpp extension
-   jasm=${2:0:$(expr ${#2} - $EXTEN_LEN)} 
+	# This operation cuts off the .cpp extension
+	jasm=${2:0:$(expr ${#2} - $EXTEN_LEN)} 
 
-   java -jar ~/bin/jasmin.jar $jasm.j > /dev/null
-else
-   echo "$0: unrecognized option '$1'"
-fi
+	java -jar ~/.clite/jasmin.jar $jasm.j > /dev/null
+
+	if [[ $1 == "-c" ]]
+	    then 
+	    rm $jasm.j
+        fi
+
+    else
+   
+	echo "$0: unrecognized option '$1'"
+    fi
 fi
